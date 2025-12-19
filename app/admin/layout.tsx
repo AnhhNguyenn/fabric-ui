0aa0db1e1991b191a49aa46463f3285c1fa4ca8f
Anh Nguyễn <138514587+AnhhNguyenn@users.noreply.github.com>
2025-12-19 11:16:17 +0700

    Delete app/admin directory

--- app/admin/layout.tsx
+++
@@ -1,5 +0,0 @@
-import AdminLayout from '../../src/components/Admin/AdminLayout';
-
-export default function AdminRootLayout({ children }: { children: React.ReactNode }) {
-  return <AdminLayout>{children}</AdminLayout>;
-}
